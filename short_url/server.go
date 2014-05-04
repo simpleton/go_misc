e2cd6bd905ad7f41c47601522e4de150147f7f6c
simpleton <sunsj1231@gmail.com>
2014-05-05 02:27:04 +0800

    redirect location

--- short_url/server.go
+++ short_url/server.go
@@ -9,8 +9,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "It Works!!! %s", r.URL.Path[1:])
 }
 
+func handler_redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "http://simsun.me", 301)
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler_redirect)
 	http.ListenAndServe(":8080", nil)
 }
 
